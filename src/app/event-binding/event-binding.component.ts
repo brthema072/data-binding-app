041d67a755d2bcc60d3397ef38f7bd54509a8a45
brthema072 <brunomarthe@hotmail.com>
2019-12-22 15:58:28 -0300

    adicionando o component event-binding

---
+++ src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.css']
+})
+export class EventBindingComponent implements OnInit {
+
+  buttonName ='My button';
+  i = 0;
+
+  spinnerMode = "determinate";
+  btnEnable = true;
+  
+  constructor() { }
+  
+  ngOnInit() {
+  }
+
+  save(){
+    console.log('click');
+  }
+
+  inc(){
+    this.i++;
+    this.buttonName ="It was clicked " +this.i+" times";
+  }
+
+  disable(){
+    this.btnEnable = false;
+    this.spinnerMode = "indeterminate";
+    setTimeout( ()=> {
+      this.btnEnable = true;
+      this.spinnerMode = "determinate";
+    }, 1000)
+  }
+}
