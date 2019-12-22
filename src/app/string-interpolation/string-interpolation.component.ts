45818edea46bdfade8b2fcd2f28c0fbb23f02b30
brthema072 <brunomarthe@hotmail.com>
2019-12-22 14:03:33 -0300

    utilizando interpolação

--- src/app/string-interpolation/string-interpolation.component.ts
+++ src/app/string-interpolation/string-interpolation.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./string-interpolation.component.css']
 })
 export class StringInterpolationComponent implements OnInit {
+  
+  firstName = "John";
+
+  age = 100;
+
+  person = {
+    firstName: "John",
+    lastName: "bro",
+    agr: 50,
+    address: "Route 100"
+  }
 
   constructor() { }
 
